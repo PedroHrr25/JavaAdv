735616ab37798ac30a5dea7c77cd0c3504036b16
Pedro Ramalho <pedro_hrr@hotmail.com>
2024-12-15 14:34:54 -0300

    predicate

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
 
-
+//Predicate
         Predicate<Integer> predicate = i -> i < 0;
         System.out.println("Predicate: " + predicate.test(-1));
         System.out.println("Predicate: " + predicate.test(+1));
+
+        String namePredicateTrue = "Mr. Joe Blogss";
+        System.out.println("Does " +namePredicateTrue + " Start with Mr. ? " + check("Mr. Joe Blogss ", s ->s.startsWith("Mr."))); //true
+        String namePredicateFalse = "Ms. Ann Blogss";
+        System.out.println("Does " +namePredicateFalse + " Start with Mr. ? " + check("Ms. Joe Blogss ", s ->s.startsWith("Mr."))); //false
+
+        Predicate<String> pStr = s -> s.contains("City");
+        System.out.println(pStr.test("Vatican City")); //True
+
+//BiPredicate
+        BiPredicate<String, Integer> checkLength = (str, len) -> str.length() == len;
+        System.out.println(checkLength.test("Vatican City",8));//false
+
 
         Evaluate<Integer> lambda = i -> {return i < 0;};
         System.out.println("Evaluate: " + lambda.isNegative(-1));
@@ -15,4 +29,9 @@ public class Main {
 
 
     }
+
+    public static <T> boolean check(T t , Predicate<T> lambda){
+        return lambda.test(t);
+    }
+
 }
